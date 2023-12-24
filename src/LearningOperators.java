e23b7429c46806e362dd248734123e2569e7c615
Ankit Khadka <khadkaankit85@gmail.com>
2023-12-23 23:16:01 -0500

    Learning operators in java

---
+++ src/LearningOperators.java
@@ -0,0 +1,23 @@
+public class LearningOperators {
+    public static void main(String[] args) {
+//        //1. Arithmetic operators
+//        int a =5;
+//        int b=6;
+//        System.out.println(a+b);
+//        System.out.println(a*b);
+//        System.out.println(a%b);
+//        System.out.println(a-b);
+//        System.out.println(a--);//prints a and the decreases the value of a by one
+//        System.out.println(--a); //decreases the value of a by one and prints it
+//        System.out.println(a);
+//        System.out.println((a++));//increases the value of a by one and prints it
+//        System.out.println(a);
+//        //2. Logical Operators
+//        System.out.println(1>0);
+//        System.out.println(1<0);
+//        System.out.println(1>0 && 5>6);//logic check==and
+//        System.out.println(1>0 || 1<0); //logic check==or
+//
+    }
+
+}
