41863a572122dd41b832e7887335dd774853edef
Ankit Khadka <khadkaankit85@gmail.com>
2023-12-24 00:37:00 -0500

    exercises done

--- src/PracticeQuestions2.java
+++ src/PracticeQuestions2.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
+
 public class PracticeQuestions2 {
     public static void main(String[] args) {
         //1. We should use float as much as possible lol:)
 //        System.out.println((7/4.0f)*(9/2.0f));
 //        System.out.println(7/4.0);
-        //2. Check if user has entered greater number than a const:)
+//        //2. Check if user has entered greater number than a const:)
+//        Scanner input =new Scanner(System.in);
+//        float a= input.nextFloat();
+//        System.out.println(a>8);
+        //3. print some formula:)
+        System.out.println(2*'a'*'s');
     }
 }
+//public class PracticeQuestions2 {
+//    public static void main(String[] args) {
+//        // Given variables
+//        String expression = "v^2 - u^2 / (2as)";
+//
+//        // Print the expression
+//        System.out.println("Expression: " + expression);
+//    }
+//}
