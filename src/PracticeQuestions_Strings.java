74bafac3396485f2824104d03d6cd87246fa8eb9
Ankit Khadka <khadkaankit85@gmail.com>
2023-12-26 13:44:10 -0500

    Strings Exercise done

---
+++ src/PracticeQuestions_Strings.java
@@ -0,0 +1,42 @@
+import java.util.Locale;
+
+public class PracticeQuestions_Strings {
+    public static void main(String[] args) {
+        String name= "Hello! I am Angkit Khadka.";
+
+        // to lowercase our string
+//        name=name.toLowerCase();
+//        System.out.println(name);
+
+        //to remove spaces
+//        name=name.trim();
+//        System.out.println(name);
+
+        //to replace space with underscore
+//        name=name.replace(" ","_");
+//        System.out.println(name);
+
+        //to format string:)
+//        String realName="Mr. Angkit Khadka";
+//        String letterTemplate ="Thank You! %s. Thanks a lot Buddy.";
+//        System.out.printf(letterTemplate,realName);
+
+//        //another way to format string
+//        String realName="Mr. Angkit Khadka";
+//        String letterTemplate ="Thank You! <|name|>. Thanks a lot Buddy.";
+//       letterTemplate=letterTemplate.replace("<|name|>",realName);
+//        System.out.println(letterTemplate);
+
+        //to know if we have double space in our string
+        //I haven't Studied if else statement, so I am using string method
+//        System.out.println(name.indexOf("  ")); //-1 means it's not in our String
+
+        //formatting using escape sequence chars
+//        System.out.println("Hello! Mr. Angkit Khadka\nShe says \"This is just attraction\nI don't Love you:( \"");
+
+
+
+
+
+    }
+}
