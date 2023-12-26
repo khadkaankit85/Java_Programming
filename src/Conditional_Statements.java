2ab312772923d47f1c8814daf28c0ae259f482e0
Ankit Khadka <khadkaankit85@gmail.com>
2023-12-26 14:31:40 -0500

    else if conditional added

--- src/Conditional_Statements.java
+++ src/Conditional_Statements.java
@@ -7,6 +7,18 @@ public class Conditional_Statements {
         if(age>18){
             System.out.println("You can drive.");
         }
+        else if(age<=17 && age>10){
+            System.out.println("A few more years to go man!");
+        }
+        else if (age==10){
+            System.out.println("Eight more Years to go pal.");
+        }
+        else if (age<0){
+            System.out.println("Age cannot be negative my man!");
+        }
+        else if (age<10){
+            System.out.println("You wanna drive? I believe You shouldn't think about driving any sooner. ");
+        }
         else{
             System.out.println("No! You cannot drive.");
         }
